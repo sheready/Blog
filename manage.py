e14756cb70ca3c32df6c7c5916b73cb432ec93b8
sheready <csstlw@gmail.com>
2020-07-25 11:10:45 +0300

    edit registration template

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Blog, Review, Role, Subscribers
 from flask_migrate import Migrate, MigrateCommand
 
 #Creating app instance
-app = create_app('production')
+app = create_app('development')
 
 
 #Manager Commands
